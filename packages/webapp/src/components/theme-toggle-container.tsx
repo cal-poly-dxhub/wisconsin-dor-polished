d76c0d7be7ef1daf5cd24e3ff0b1884e99ecd095
spagdoon0411 <spandan.suthar@live.com>
2025-08-26 17:22:47 -0700

    Define global theme toggle

---
+++ packages/webapp/src/components/theme-toggle-container.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ThemeToggle } from "./theme-toggle";
+
+export function ThemeToggleContainer() {
+  return (
+    <div className="fixed top-4 right-4 z-50">
+      <ThemeToggle />
+    </div>
+  );
+}
