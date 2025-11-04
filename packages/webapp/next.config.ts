3714f84a58baccf88c7c4768a1bcecc3339a409c
spagdoon0411 <spandan.suthar@live.com>
2025-11-04 06:29:15 -0800

    tmp: try transpile trick

--- packages/webapp/next.config.ts
+++ packages/webapp/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    esmExternals: 'loose',
+  },
+  transpilePackages: [
+    'flowtoken',
+    'remark-gfm',
+    'micromark',
+    'micromark-core-commonmark',
+    'micromark-extension-gfm',
+    'micromark-extension-gfm-footnote',
+    'decode-named-character-reference',
+    'character-entities',
+  ],
+  webpack(config) {
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.mjs', '.cjs'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
