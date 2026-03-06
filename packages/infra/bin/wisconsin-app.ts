bb72b87aece0b2a135ba8f0d82ba45ec73318a65
spagdoon0411 <spandan.suthar@live.com>
2026-03-06 13:28:51 -0800

    Restore stack ID

--- packages/infra/bin/wisconsin-app.ts
+++ packages/infra/bin/wisconsin-app.ts
@@ -4,7 +4,7 @@ import { WisconsinBotStack } from '../lib/stack';
 
 const app = new cdk.App();
 
-new WisconsinBotStack(app, 'WisconsinBotStackMergedRag', {
+new WisconsinBotStack(app, 'WisconsinBotStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
